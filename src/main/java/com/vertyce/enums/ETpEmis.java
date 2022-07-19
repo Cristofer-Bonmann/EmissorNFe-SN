97939e68429f70e08424bc8891bf14436f905460
Cristofer Bonmann <cristoferbonmann@gmail.com>
2022-07-19 01:51:22 -0300

    Adicionado Doc nas classes enumerations.

--- src/main/java/com/vertyce/enums/ETpEmis.java
+++ src/main/java/com/vertyce/enums/ETpEmis.java
@@ -1,5 +1,26 @@
 package com.vertyce.enums;
 
+/**
+ * Tipo de Emissão da NF-e. Usar para a TAG tpEmis  <br>
+ *
+ * 1=Emissão normal (não em contingência);  <br>
+ * 2=Contingência FS-IA, com impressão do DANFE em formulário
+ * de segurança;  <br>
+ * 3=Contingência SCAN (Sistema de Contingência do Ambiente
+ * Nacional);  <br>
+ * 4=Contingência DPEC (Declaração Prévia da Emissão em
+ * Contingência);  <br>
+ * 5=Contingência FS-DA, com impressão do DANFE em
+ * formulário de segurança;  <br>
+ * 6=Contingência SVC-AN (SEFAZ Virtual de Contingência do
+ * AN);  <br>
+ * 7=Contingência SVC-RS (SEFAZ Virtual de Contingência do
+ * RS);  <br>
+ * 9=Contingência off-line da NFC-e (as demais opções de
+ * contingência são válidas também para a NFC-e).
+ * Para a NFC-e somente estão disponíveis e são válidas as
+ * opções de contingência 5 e 9.
+ */
 public enum ETpEmis {
 
     NORMAL("1", "Emissão normal (não em contingência)"),
--- src/main/java/com/vertyce/enums/ETpImp.java
+++ src/main/java/com/vertyce/enums/ETpImp.java
@@ -1,5 +1,18 @@
 package com.vertyce.enums;
 
+/**
+ * Formato da impressão da DANFE. Usar para a TAG tpImp <br>
+ *
+ * 0=Sem geração de DANFE; <br>
+ * 1=DANFE normal, Retrato; <br>
+ * 2=DANFE normal, Paisagem; <br>
+ * 3=DANFE Simplificado; <br>
+ * 4=DANFE NFC-e; <br>
+ * 5=DANFE NFC-e em mensagem eletrônica (o envio de
+ * mensagem eletrônica pode ser feita de forma simultânea com a
+ * impressão do DANFE; usar o tpImp=5 quando esta for a única
+ * forma de disponibilização do DANFE). <br>
+ */
 public enum ETpImp {
     SEM_DANFE("0", "Sem geração de DANFE"),
     RETRATO("1", "DANFE normal, Retrato"),
