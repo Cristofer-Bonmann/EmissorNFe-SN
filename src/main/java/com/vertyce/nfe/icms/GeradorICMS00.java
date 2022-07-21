f40e04666dd398ab54e3e27faa34e243e6d93809
Cristofer Bonmann <cristoferbonmann@gmail.com>
2022-07-21 15:21:32 -0300

    Adicionada JavaDoc.

--- src/main/java/com/vertyce/nfe/icms/GeradorICMS00.java
+++ src/main/java/com/vertyce/nfe/icms/GeradorICMS00.java
@@ -1,13 +1,19 @@
 package com.vertyce.nfe.icms;
 
 import br.com.swconsultoria.nfe.schema_4.enviNFe.TNFe;
+import br.com.swconsultoria.nfe.schema_4.enviNFe.TNFe.InfNFe.Det.Imposto.ICMS;
 import com.vertyce.enums.EModBC;
 
-import javax.xml.bind.JAXBElement;
 import java.util.List;
 
 public class GeradorICMS00 implements com.vertyce.nfe.icms.GeradorICMS00Presenter {
-    // TODO: 20/07/2022 inserir doc
+    /**
+     * Gera os informações para a tributação do ICMS 00. <br>
+     * O objeto Imposto deve ter pelo menos um item na sua lista de Content(lista JAXBElement); <br>
+     * O valor do primeiro item do Content (lista JAXBElement) do Imposto deve ser diferente de null e deve ser do tipo
+     * TNFe.InfNFe.Det.Imposto.ICMS<br>
+     * @param infNFe
+     */
     @Override
     public void geraICMS00(TNFe.InfNFe infNFe) {
         List<TNFe.InfNFe.Det> dets = infNFe.getDet();
@@ -22,9 +28,9 @@ public class GeradorICMS00 implements com.vertyce.nfe.icms.GeradorICMS00Presente
                     return valueIcms;
 
                 })
-                .filter(valueIcms -> valueIcms instanceof TNFe.InfNFe.Det.Imposto.ICMS)
+                .filter(valueIcms -> valueIcms instanceof ICMS)
                 .map(valueIcms -> {
-                    TNFe.InfNFe.Det.Imposto.ICMS icms = (TNFe.InfNFe.Det.Imposto.ICMS) valueIcms;
+                    ICMS icms = (ICMS) valueIcms;
                     return icms;
 
                 }).forEach(icms -> {
@@ -35,7 +41,7 @@ public class GeradorICMS00 implements com.vertyce.nfe.icms.GeradorICMS00Presente
                     final String pICMS = "10.0000";
                     final String vICMS = "90.00";
 
-                    TNFe.InfNFe.Det.Imposto.ICMS.ICMS00 icms00 = new TNFe.InfNFe.Det.Imposto.ICMS.ICMS00();
+                    ICMS.ICMS00 icms00 = new ICMS.ICMS00();
                     icms.setICMS00(icms00);
 
                     icms00.setOrig(orig);
